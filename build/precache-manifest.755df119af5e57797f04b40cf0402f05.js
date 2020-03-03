941a85423aade6a637d532449923b5ea92f43e03
Sunil kumar M <sunil.kumar@piktorlabs.com>
2020-03-04 02:07:38 +0530

    init commit

---
+++ build/precache-manifest.755df119af5e57797f04b40cf0402f05.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7e9c95a22235351ffc12d44ba04da3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b06c5e223fef54168e98",
+    "url": "/static/css/main.0cc00f5a.chunk.css"
+  },
+  {
+    "revision": "cd3f86b419dc1582052b",
+    "url": "/static/js/2.2ab735ce.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.2ab735ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06c5e223fef54168e98",
+    "url": "/static/js/main.cd1e9599.chunk.js"
+  },
+  {
+    "revision": "5fd3875ae3fc0ac4e7a7",
+    "url": "/static/js/runtime-main.07d33041.js"
+  },
+  {
+    "revision": "09c3413e9fb6831ffddabfc417a7ecc4",
+    "url": "/static/media/alexandru-acea-GhwCef9VRr4-unsplash.09c3413e.jpg"
+  },
+  {
+    "revision": "d3d5088f6491df12be40d840fcc12e2d",
+    "url": "/static/media/facebook.d3d5088f.svg"
+  },
+  {
+    "revision": "15bc5470aea6d2d05200dff45c14e92b",
+    "url": "/static/media/instagram.15bc5470.svg"
+  },
+  {
+    "revision": "ab46037f2bb51cacbd0e7bea5a8cf3a6",
+    "url": "/static/media/linkedin.ab46037f.svg"
+  },
+  {
+    "revision": "1eb3dcd44b71e1963796f05fe6e8b01f",
+    "url": "/static/media/simon-abrams-k_T9Zj3SE8k-unsplash.1eb3dcd4.jpg"
+  }
+]);
